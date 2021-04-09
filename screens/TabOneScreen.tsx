eb0a627f6fa49e16330da8bb796aa0aa88d076b3
super2pacc <nicolas.paccard@gadz.org>
2021-04-09 20:12:05 +0200

    Add the logo on the tab one screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,7 +7,8 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One TOTO</Text>
+      <Text style={styles.title}>Bienvenue sur Baryc ! :)</Text>
+      <Image style={styles.images} source={require("../assets/images/beer.png")}/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -29,4 +30,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  images:{
+
+    width : 100,
+    height : 100,
+  }
 });

c2b53a72fe6a36d228fc7c74a116bfdac0a8363f
Germain <2016-1044@ensam.eu>
2021-04-13 21:40:32 +0200

    Fixes

--- helpers/API/BarsAPI.tsx
+++ helpers/API/BarsAPI.tsx
@@ -5,7 +5,6 @@ export async function getBarsFromApi() {
     const path = "/bars";
     const url =
         "https://f3n48sbbli.execute-api.eu-west-1.amazonaws.com/v1" + path;
-    const bars: Object[] = []
     return await axios
     .get(url, {
         headers: {
@@ -13,7 +12,7 @@ export async function getBarsFromApi() {
         }
     })
     .then(response =>
-        response = JSON.parse(response.data.body)
+        JSON.parse(response.data.body)
     )
     .catch((error) => console.error(error)) // TODO : errors should be displayed to user with a component conditionnaly displayed by a Redux store state variable
 }

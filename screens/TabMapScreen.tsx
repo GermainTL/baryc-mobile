d9ece76dc55979bcd0984dd9429f3e04b582dd71
Germain <2016-1044@ensam.eu>
2021-04-22 22:59:44 +0200

    Add minimum search bars and button to card at bottom of map

--- screens/TabMapScreen.tsx
+++ screens/TabMapScreen.tsx
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Dimensions, ScrollView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { Icon } from "react-native-elements";
+import { Icon, Card, SearchBar, ButtonGroup } from "react-native-elements";
 
 import { parisLocalization } from "~/constants/GPSConstants.ts";
 import { getBarsFromApi } from "~/helpers/API/BarsAPI.tsx";
-import { default as palette } from "~/constants/Colors.ts" ;
+import palette from "~/constants/Colors.ts" ;
 
 export default class TabMapScreen extends Component {
   constructor(){
     super();
     this.state= {
       markers: [],
-      isLoading: true
+      isLoading: true,
+      showSearchPanel: false,
+      locations: [
+          {
+            searchValue: '',
+            location: {
+              latitude: null,
+              longitude: null
+            }
+          },
+          {
+            searchValue: '',
+            location: {
+              latitude: null,
+              longitude: null
+            }
+          },
+      ],
+      selectedTransport: null
     }
   }
 
@@ -31,40 +49,80 @@ export default class TabMapScreen extends Component {
       }
       this.setState({
         markers: markers,
-        isLoading: false
+        isLoading: false,
       });
     })
   }
+  
+  togglePanel(): void {
+    this.setState((prevState) => {
+      return {
+        showSearchPanel: prevState.showSearchPanel !== true
+      }
+    });
+  }
+
+  updateSelectedOption(selectedOption): void {
+    this.setState({ selectedTransport: selectedOption})
+  }
+
+  handleSearch(searchText, index): void {
+    const locations = [ ...this.state.locations ];
+    const location = { ...locations[index] };
+    location.searchValue = searchText;
+    locations[index] = location;
+    this.setState({ locations});
+  }
 
   render(): JSX.Element {
-    const { markers, isLoading } = this.state;
-
+    const { markers, isLoading, showSearchPanel, selectedTransport } = this.state;
+    const transportOptions = ['walk', 'transport']
+    
     return (
-        <View style={{ flex: 1 }}>
-        <MapView
-            style={ styles.map }
-            initialRegion={ parisLocalization }
-            showsUserLocation={ true }
-        >
-          {
-            isLoading === false && (
-              markers.map((marker) => {
-              return  (
-                  <Marker
-                      coordinate={ marker.coordinates }
-                      title={ marker.title }
-                      key={ marker.key }
-                  >
-                    <Icon name="map-marker" type="font-awesome" color={ palette.barycOrangeLight } style={ palette.iconShadow }/>
-                  </Marker>
+        <View style={ styles.container }>
+          <MapView
+              style={ styles.map }
+              initialRegion={ parisLocalization }
+              showsUserLocation={ true }
+          >
+            {
+              isLoading === false && (
+                markers.map((marker) => {
+                return  (
+                    <Marker
+                        coordinate={ marker.coordinates }
+                        title={ marker.title }
+                        key={ marker.key }
+                    >
+                      <Icon name="map-marker" type="font-awesome" color={ palette.barycOrangeLight } style={ palette.iconShadow }/>
+                    </Marker>
+                )
+               })
               )
-             })
+            }
+          </MapView>
+
+          <TouchableOpacity style={ styles.searchButton } onPress={() => this.togglePanel() }>
+            <Icon name="search-outline" reverse type="ionicon" color="white" reverseColor="black"/>
+          </TouchableOpacity>
+
+          {
+            showSearchPanel === true && (
+                <ScrollView onScroll={() => this.togglePanel()} style={ styles.searchPanel } scrollEventThrottle={0}>
+                  <Card containerStyle={ styles.card } >
+                    <View>
+                      <SearchBar onChangeText={(text) => this.handleSearch(text, 0) } value={ this.state.locations[0].searchValue } lightTheme containerStyle={ styles.searchBar } />
+                      <SearchBar onChangeText={(text) => this.handleSearch(text, 1) } value={ this.state.locations[1].searchValue } lightTheme containerStyle={ styles.searchBar } />
+                      <ButtonGroup
+                          onPress={(value) => this.updateSelectedOption(transportOptions[value]) }
+                          selectedIndex={ selectedTransport }
+                          buttons={ transportOptions }
+                      />
+                    </View>
+                  </Card>
+                </ScrollView>
             )
           }
-        </MapView>
-        <TouchableOpacity style={ styles.searchButton }>
-          <Icon name="search-outline" reverse type="ionicon" color="white" reverseColor="black" style={ palette.iconShadow } />
-        </TouchableOpacity>
         </View>
     );
   }
@@ -72,6 +130,10 @@ export default class TabMapScreen extends Component {
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative"
+  },
   map: {
     flex: 1,
     alignItems: 'center',
@@ -79,16 +141,33 @@ const styles = StyleSheet.create({
     borderRadius: 3,
   },
   searchButton: {
+    backgroundColor: 'transparent',
     flex: 1,
     position: 'absolute',
     bottom: 0,
     right: 0,
     color: "white",
     shadowColor: '#000',
-    shadowOffset: {
-      width: 0, height: 10
-    },
+      shadowOffset: {
+        width: 0, height: 10
+      },
     shadowOpacity: 0.2,
-    elevation: 1
-  }
+    elevation: 1,
+    },
+    searchPanel: {
+      flex: 1,
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      width: Dimensions.get('window').width
+    },
+    card: {
+      marginLeft: 0,
+      marginRight: 0,
+    },
+    searchBar: {
+      backgroundColor: "white",
+      borderTopColor: "white",
+      borderBottomColor: "white"
+    },
 });

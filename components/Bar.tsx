9b55a29404a811e631711da66cf37f3167c26993
super2pacc <nicolas.paccard@gadz.org>
2021-05-17 22:00:18 +0200

    Transform the bar function to class

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,30 +1,31 @@
 import { Text, View, StyleSheet } from 'react-native';
-import * as React from 'react';
 import { Card, Rating } from 'react-native-elements'
+import React, { Component } from 'react';
 
-interface Props {
-    bar: Object;
-}
 
-export function Bar(props: Props) {
-    return (
+
+export default class Bar extends Component {
+    render(): JSX.Element {
+        return (
             <Card containerStyle={ styles.card }>
                 <View style={ styles.titleContainer }>
                     <Text style={ styles.title }>
                     {
-                        props.bar.nom
+                        this.props.bar.nom
                     }
                     </Text>
-                    <Rating fractions={ 1 } readonly imageSize={ 16 } startingValue={ props.bar.note }/>
+                    <Rating fractions={ 1 } readonly imageSize={ 16 } startingValue={ this.props.bar.note }/>
                 </View>
                 <Text style={ styles.description }>
                     "{
-                        props.bar.renseignements
+                        this.props.bar.renseignements
                     }"
                 </Text>
             </Card>
-    )
+        );
+    }
 }
+
 
 
 const styles = StyleSheet.create({

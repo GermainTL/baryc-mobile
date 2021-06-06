a2894ff04eead7cf8d742ce281e97a2f640aeef6
Germain <2016-1044@ensam.eu>
2021-06-07 00:02:47 +0200

    Give my identifier apple for bundling

--- helpers/API/NavitiaAPI.tsx
+++ helpers/API/NavitiaAPI.tsx
@@ -2,44 +2,38 @@ import axios from "axios";
 import { API_TOKEN_NAVITIA, HTTPS_PROTOCOL } from "@env";
 import { reformatCoordinates } from "../CoordinatesHelper.tsx";
 
-function getIsochroneCoordinates(minutes: Number, coordinates: Object): Promise {
-    return new Promise( (resolve, reject) => {
+async function getIsochroneCoordinates(minutes: Number, coordinates: Object) {
         const urlNavitia =
             `${HTTPS_PROTOCOL}api.navitia.io/v1/coverage/fr-idf/isochrones?from=${coordinates.latitude};${coordinates.longitude}&min_duration=0&max_duration=${minutes * 60}`
-        axios
-            .get(urlNavitia, {
-                headers: {
-                    Authorization: API_TOKEN_NAVITIA,
-                },
-            })
-            .then((response) =>
-                resolve({
-                    coordinates: response.data.isochrones[0].geojson.coordinates, // useful for intersection compute
-                    reformattedCoordinates: reformatCoordinates(JSON.parse(JSON.stringify(response.data.isochrones[0].geojson.coordinates))), // useful for drawing multiPolygons
-                })
-            )
-            .catch((error) => {
-                reject(error);
-            });
-    })
+        return await axios
+                    .get(urlNavitia, {
+                        headers: {
+                            Authorization: API_TOKEN_NAVITIA,
+                        },
+                    })
 }
 
 function getIsochronesCoordinates(locations: any[], travelTime: Number): Promise {
-    return new Promise( async resolve => {
-        const getIsochronesCoordinates = []
+        const isochronesCoordinatesPromises = []
         for (const index in locations) {
             if (locations[index].GPSPosition.latitude !== null) {
-                getIsochronesCoordinates.push(getIsochroneCoordinates(travelTime, locations[index].GPSPosition))
+                isochronesCoordinatesPromises.push(getIsochroneCoordinates(travelTime, locations[index].GPSPosition))
             }
         }
-        Promise.all(getIsochronesCoordinates) // Promise are never resolved, why ?
-            .then((newIsochronesCoordinates) => {
-                resolve(newIsochronesCoordinates)
-             })
-            .catch((error) => {
-                console.error(error);
-            });
-    })
+        return Promise.all(isochronesCoordinatesPromises) // Promise are never resolved, why ?
+                .then((apiResponses) => {
+                    const newIsochronesCoordinates = []
+                    for (const apiResponse of apiResponses) {
+                        newIsochronesCoordinates.push({
+                            coordinates: apiResponse.data.isochrones[0].geojson.coordinates, // useful for intersection compute
+                            reformattedCoordinates: reformatCoordinates(JSON.parse(JSON.stringify(apiResponse.data.isochrones[0].geojson.coordinates))), // useful for drawing multiPolygons
+                        })
+                    }
+                    return newIsochronesCoordinates
+                 })
+                .catch((error) => {
+                    console.error(error);
+                });
 }
 
 
